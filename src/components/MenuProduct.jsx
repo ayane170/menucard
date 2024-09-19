a873baff797860a3a2fec82c57ccfa74a470640d
Aya Nemri <r0987683@student.thomasmore.be>
2024-09-19 09:46:58 +0200

    Exercise: Styling css-in-js

--- src/components/MenuProduct.jsx
+++ src/components/MenuProduct.jsx
@@ -1,11 +1,17 @@
 export function MenuProduct(props) {
     const {product} = props;
-    if (!product?.name || !product?.price )return null;
+    if (!product?.name  )return ;
     return (
-        <div className="product-item">
+        <div style={{margin: "2vw", fontSize: "larger", display: "flex"}}>
+            <div style={{flex: 1}}>
             {product.name}
-            {product.size ? ` (${product.size} cl)` : '--' }
-            {product.price} &euro;
-        </div>
-    );
-}
+                <span style={{color:"blue"}}>
+            {product.size ? ` (${product.size} cl)` : ''}
+                </span>
+            </div>
+            <div style={{flex: 1}}>
+                {product.price} &euro;
+            </div>
+            </div>
+            );
+            }
