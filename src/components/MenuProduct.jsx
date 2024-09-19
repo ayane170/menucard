9d83b08108dd03edbb497aff2262f65e074964b9
Aya Nemri <r0987683@student.thomasmore.be>
2024-09-19 09:52:06 +0200

    Exercise: extra info notes

--- src/components/MenuProduct.jsx
+++ src/components/MenuProduct.jsx
@@ -8,6 +8,9 @@ export function MenuProduct(props) {
                 <div style={{color:"blue" ,     display:"inline"}}>
             {product.size ? ` (${product.size} cl)` : ''}
                 </div>
+                <div style={{color:"blue" , fontSize:14}}>
+                    {product.note}
+                </div>
             </div>
             <div style={{flex: 1}}>
                 {product.price} &euro;
--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,8 @@
 export const PRODUCTS_DATA = [
-    {name:"cola" , price:1},
-    {name:"water" , price:1},
-    {name:"bier" , price:1 , size:25},
-    {name:"wijn" , price:2 , size:33},
-    {name:"cava" , price:3},
+    {name: "cola", price: 1 },
+    {name: "water", price: 1, note: "plat of bruisend"},
+    {name: "bier", price: 1, size: 25},
+    {name: "bier groot", price: 1, size: 33},
+    {name: "wijn", price: 2, note:"rood, wit of rosé"},
+    {name: "cava", price: 3},
 ]
