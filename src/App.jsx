c7508046c4a1a855f6863c264d4c2c6645dbc476
Aya Nemri <r0987683@student.thomasmore.be>
2024-09-19 12:14:42 +0200

    Exercise:Tab Component (1.5)

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,28 @@ import './App.css'
 import {MenuCardPage} from "./pages/MenuCardPage.jsx";
 import {PRODUCTS_DATA} from "./data/data.js";
 import {PicturesPage} from "./pages/PicturesPage.jsx";
+import {Tab, TabList, TabPanel, Tabs} from "react-tabs";
+
 
 function App() {
     return (
-            <>
+        <>
+            <Tabs>
+                <TabList>
+                    <Tab>menu</Tab>
+                    <Tab>pics</Tab>
+                </TabList>
+                <TabPanel>
+                    <MenuCardPage products={PRODUCTS_DATA}/>
+                </TabPanel>
+                <TabPanel>
+                    <PicturesPage/>
+                </TabPanel>
+            </Tabs>
 
-                {/*{PRODUCTS_DATA.map(p => <MenuProduct key={p.name} product = {p}/>)}*/}
-                <MenuCardPage products = {PRODUCTS_DATA}/>
-                <PicturesPage/>
-            </>
+            {/*{PRODUCTS_DATA.map(p => <MenuProduct key={p.name} product = {p}/>)}*/}
+
+        </>
 
     )
 }
--- src/pages/PicturesPage.jsx
+++ src/pages/PicturesPage.jsx
@@ -1,5 +1,8 @@
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 export function PicturesPage() {
     return (
+
         <div>
             <h1>Pictures</h1>
         </div>
