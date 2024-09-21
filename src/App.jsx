d7c9a8af7fd9f607be2c7958d1c4b0d33bbdf6bd
Aya Nemri <r0987683@student.thomasmore.be>
2024-09-21 13:21:03 +0200

    react-Bootstrap installeer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "normalize.css"
 import './App.css'
 // import {MenuProduct} from "./components/MenuProduct.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {MenuCardPage} from "./pages/MenuCardPage.jsx";
 import {PRODUCTS_DATA} from "./data/data.js";
 import {PicturesPage} from "./pages/PicturesPage.jsx";
