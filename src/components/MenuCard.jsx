636e886444fbc4b6144ac98c77642d503e357a85
Aya Nemri <r0987683@student.thomasmore.be>
2024-09-21 13:56:44 +0200

    Exercise: Gebruik react-bootstrap components

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,10 +1,16 @@
 import {MenuProduct} from "./MenuProduct.jsx";
+import {Container,Row,Col} from "react-bootstrap";// Importer les composants de react-bootstrap
+
 // import {PRODUCTS_DATA} from "../data/data.js";
 
 export function MenuCard(props){
     const {products} = props;
     return (
-        <div>
-            {products.map(p => <MenuProduct key={p.name} product={p}/>)}
-        </div>);
+        <Container>
+            <Row>
+                {products.map(p =>(<Col key={p.name} xs={12} sm={6} md={4} lg={3} className="mb-4">)
+                    <MenuProduct key={p.name} product={p}/></Col>
+                ))}
+            </Row>
+        </Container>);
 }
