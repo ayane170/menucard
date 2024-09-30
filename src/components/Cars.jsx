a440243906a6b12818766806068a9e2b3b2ae1f2
Aya Nemri <r0987683@student.thomasmore.be>
2024-09-30 22:09:14 +0200

    Car color

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -1,39 +1,67 @@
-import { Container, Row, Col } from "react-bootstrap";
-import Card from "react-bootstrap/Card";
+import PropTypes from "prop-types";
+import {Card, Col, Container, Row} from "react-bootstrap";
 
-// Composant Car pour afficher chaque voiture
-function Car({ car }) {
+const COLORS_CAR_CSS = [
+    {nameInCar: "blauw", backGroundColorInCss: "blue", colorInCss: "white"},
+    {nameInCar: "geel", backGroundColorInCss: "yellow"},
+    {nameInCar: "zwart", backGroundColorInCss: "black", colorInCss: "white"},
+    {nameInCar: "wit", backGroundColorInCss: "white"},
+    {nameInCar: "rood", backGroundColorInCss: "red", colorInCss: "white"},
+    {nameInCar: "grijs", backGroundColorInCss: "grey", colorInCss: "white"},
+    {nameInCar: "groen", backGroundColorInCss: "green", colorInCss: "white"},
+];
+
+//merk op: dit is een gewone functie, geen React-Component
+function styleColor(color) {
+    const colorCarCss = COLORS_CAR_CSS.find(c => c.nameInCar === color);
+    if (!colorCarCss) return;
+    return {
+        backgroundColor: colorCarCss.backGroundColorInCss,
+        color: colorCarCss.colorInCss,
+        margin: "0.5em"
+    };
+}
+
+function CarColor(props) {
+    const {color} = props;
+
+    if (!color) return null;
+    return <div style={styleColor(color)}>
+        kleur: {color} &nbsp;
+    </div>;
+}
+
+function Car(props) {
+    const {car} = props;
     return (
-        <Card className="m-2 p-2 shadow-sm">
-            <Card.Body>
-                {car.name && <p><strong>{car.name}</strong> </p>}
-                {/* Afficher 'Brand' uniquement s'il existe */}
-                {car.brand && <p><strong>Type:</strong> {car.brand}</p>}
-                {/* Afficher 'Type' uniquement s'il existe */}
-                {car.type && <p><strong>Type:</strong> {car.type}</p>}
-                {/* Afficher 'Kleur' uniquement s'il existe */}
-                {car.color && <p><strong>Kleur:</strong> {car.color}</p>}
-
-            </Card.Body>
-        </Card>
+        <Col key={car.id}
+             xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+             className={"d-flex align-items-stretch text-center "}>
+            <Card className="m-2 p-2 shadow-sm">
+                <h5>{car.name}</h5>
+                {car.brand && <div>merk: {car.brand}</div>}
+                {car.type && <div>type: {car.type}</div>}
+                <CarColor color={car.color}/>
+            </Card>
+        </Col>
     );
 }
 
-// Composant Cars pour afficher la liste des voitures
 export function Cars(props) {
-    const {cars, title} = props;
+    const {cars, title} = props
     return (
-        <div className="mt-3 rounded shadow-sm" style={{ background: "lavender" }}>
+        <div className="mt-3 rounded shadow-sm " style={{backgroundColor: "lavender"}}>
             <h2 className="text-center">{title}</h2>
-            <Container>
-                <Row>
-                    {cars.map((car) => (
-                        <Col xs={12} sm={6} md={4} lg={3} key={car.id} className="text-center">
-                            <Car car={car} /> {/* Utilisation de Car pour afficher chaque voiture */}
-                        </Col>
-                    ))}
+            <Container fluid className="mt-3" >
+                <Row className="g-3">
+                    {cars.map(c => <Car key={c.id} car={c}/>)}
                 </Row>
             </Container>
         </div>
     );
 }
+
+Cars.propTypes = {
+    cars: PropTypes.array,
+    title: PropTypes.string
+};
--- src/pages/CarsPage.jsx
+++ src/pages/CarsPage.jsx
@@ -1,15 +1,15 @@
-import Card from "react-bootstrap/Card";
 import {Cars} from "../components/Cars.jsx";
 
 export function  CarsPage(props){
     const {cars} = props
 
     return(
-        <Card style={{ padding: '20px', margin: '20px', border: '1px solid #ddd' }}>
-            <Card.Body>
+        <div className="mx-3" >
+
                 <Cars cars={cars} title={"Auto's"} />
-            </Card.Body>
-        </Card>
+        </div>
+
+
     )
 }
 
