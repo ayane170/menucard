caf7be426653683e3931442e353824132ee906fb
Aya Nemri <r0987683@student.thomasmore.be>
2024-10-02 19:23:19 +0200

    show order Personsnog functie

--- src/pages/PersonsPage.jsx
+++ src/pages/PersonsPage.jsx
@@ -1,4 +1,5 @@
 import {Persons} from "../components/Persons.jsx";
+import {Numbers} from "../components/Numbers.jsx";
 
 export function PersonsPage(props){
     const {persons}= props;
@@ -7,6 +8,9 @@ export function PersonsPage(props){
             <Persons persons={persons} title="alle personen"/>
             <Persons persons={persons.toSorted((p1,pr) => pr.age - p1.age)} title="Volgend leeftijd"/>
             <Persons persons={persons.toSorted((p1, p2) => p1.city.localeCompare(p2.city))} title="Volgend stad" />
+            <Numbers numbers={persons.map(p => p.age)} title="alle personen"/>
+            <Numbers numbers={[...new Set(persons.map(p => p.age))].sort((n1, n2) => n2 - n1)}
+                     title="leeftijden gesorteerd en uniek"/>
 
         </div>
     );
