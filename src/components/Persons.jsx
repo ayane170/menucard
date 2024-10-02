d1e841578917d827543af40daea3099ea1242893
Aya Nemri <r0987683@student.thomasmore.be>
2024-10-02 19:09:53 +0200

    show order Persons bij leeftijd en bij steden

--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -17,9 +17,11 @@ function Person(props){
 export function Persons(props){
     const {persons,title} = props;
     return(
+
         <Section title={title}>
             {persons.map(p => <Person key={p.id} person={p}  />)}
         </Section>
+
     );
 
 
--- src/pages/PersonsPage.jsx
+++ src/pages/PersonsPage.jsx
@@ -5,6 +5,9 @@ export function PersonsPage(props){
     return(
         <div>
             <Persons persons={persons} title="alle personen"/>
+            <Persons persons={persons.toSorted((p1,pr) => pr.age - p1.age)} title="Volgend leeftijd"/>
+            <Persons persons={persons.toSorted((p1, p2) => p1.city.localeCompare(p2.city))} title="Volgend stad" />
+
         </div>
     );
 }
