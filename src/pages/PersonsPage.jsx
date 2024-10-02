d6f86a628f56099f17172b638848bcc4d6ba695b
Aya Nemri <r0987683@student.thomasmore.be>
2024-10-02 20:47:26 +0200

    cities from persondata

--- src/pages/PersonsPage.jsx
+++ src/pages/PersonsPage.jsx
@@ -1,6 +1,18 @@
 import {Persons} from "../components/Persons.jsx";
 import {Numbers} from "../components/Numbers.jsx";
 
+
+function citiesFromPersonData(persons){
+    const uniqueCityNames = [...new Set(persons.map(p => p.city))];
+    const cities = uniqueCityNames.map(c => ({
+        name: c,
+        numberOfPersons: persons.filter(p => p.city === c).length
+    }));
+    console.log(cities)
+    return cities;
+}
+
+
 export function PersonsPage(props){
     const {persons}= props;
     return(
