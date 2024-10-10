9a258442f6a602fb302b602046dcc359a1f519c1
Aya Nemri <r0987683@student.thomasmore.be>
2024-10-10 12:25:29 +0200

    Exercise stopPropagation()

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -13,6 +13,20 @@ export function EventDemo(props){
         console.log("link is clicked");
     };
 
+    const handleDivClick = () => {
+        console.log("div is clicked"); // Message pour la div
+    };
+    // Gestionnaire de clic pour le bouton
+    const handleButtonClick = (event) => {
+        event.stopPropagation(); // Empêche la propagation de l'événement vers la div parente
+        handleClick(event); // Appelle la fonction de clic du bouton
+    };
+    const myStyle ={
+        div:{
+            background:"#33ffec",
+        }
+    }
+
     return (
         <>
         <Section title={title}>
@@ -30,6 +44,14 @@ export function EventDemo(props){
                     <a   href="https://example.com" onClick={linkClick}>
                         dit is een link met een href
                     </a>
+            </Section>
+            <Section title="stop propagation">
+                <div  style={myStyle.div} onClick={handleDivClick}>
+                    dit is een div.
+                    <Button onClick={handleButtonClick} variant="primary">
+                        dit is een button
+                    </Button>
+                </div>
             </Section>
         </>
     );
