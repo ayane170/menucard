46f24513f4fb610662bca8d94b6c0a53d9bddb9b
Aya Nemri <r0987683@student.thomasmore.be>
2024-10-10 12:09:21 +0200

    Exercise preventDefault()

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+// import React from "react";
 import {Section} from "../components/Section.jsx";
 import {Button} from "react-bootstrap";
 
+
 export function EventDemo(props){
     const {title} = props;
-    const handleClick = () => {
+    const handleClick = (event) => {
+        event.preventDefault();
         console.log("Button is geklikt!");
+    };
+    const linkClick = () =>{
+        console.log("link is clicked");
     };
 
     return (
+        <>
         <Section title={title}>
             <div className="d-grid gap-2">
                 <button  onClick={handleClick} >
@@ -18,10 +24,14 @@ export function EventDemo(props){
                     click me please!
                 </Button>
             </div>
-            {/*<button onClick={handleClick}>click me please!</button>*/}
-            {/*<button onClick={handleClick}>click me please!</button>*/}
-
         </Section>
+            <Section title="Prevent Default" >
+                <div>Dit is een div(een section).</div>
+                    <a   href="https://example.com" onClick={linkClick}>
+                        dit is een link met een href
+                    </a>
+            </Section>
+        </>
     );
 };
 
