0b0af0e73f3da025064403ce49cfe99d2550e395
Aya Nemri <r0987683@student.thomasmore.be>
2024-10-12 13:58:26 +0200

    open/close Section Component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -16,26 +16,37 @@
 // }
 
 import {Button, Container, Row} from "react-bootstrap";
-
+import {useState} from "react";
 
 export function Section(props) {
     const {title, children} = props;
+    const [isOpen,setIsOpen]=useState(false); // il a une relation avec la function de la ligne 25
 
-    const handleClick = () => {
+    const toggleSection = () => {
+        setIsOpen(!isOpen);
         console.log(`Section ${title} is clicked`)
     }
+
+    // const handleClick = () => {
+    //     console.log(`Section ${title} is clicked`)
+    // }
 
     return (
         <div className="mt-3 rounded shadow-sm" style={{backgroundColor: "lavender"}}>
             <div className="d-flex justify-content-center align-items-center px-3 mb-3" style={{gap: '10px'}}>
                 <h2 className="mb-0">{title}</h2>
-                <Button onClick={handleClick}>Click!</Button>
+                {/*<Button onClick={handleClick}>Click!</Button>*/}
+                <Button onClick={toggleSection}>
+                    {isOpen ? "-" : "+"}
+                </Button>
             </div>
-            <Container>
-                <Row>
-                    {children}
-                </Row>
-            </Container>
+            {isOpen && (
+                <Container>
+                    <Row>
+                        {children}
+                    </Row>
+                </Container>
+            )}
         </div>
     );
 }
