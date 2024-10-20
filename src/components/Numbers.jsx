8fdce8d086863eb8278eca6833a63aa0b3abc838
Aya Nemri <r0987683@student.thomasmore.be>
2024-10-20 14:12:38 +0200

    favorite number stap 3

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -3,11 +3,15 @@ import {SectionCard} from "./SectionCard.jsx";
 
 
 export function Numbers(props) {
-    const {numbers, title,onSelectNumber,isInitiallyOpen} = props;
+    const {numbers, title,onSelectNumber,isInitiallyOpen, markedNumbe} = props;
     return (
         <Section title={title} isInitiallyOpen={isInitiallyOpen}>
             {numbers.map((num, i) => (
-                <SectionCard key={i} onSelect={onSelectNumber && (() => onSelectNumber(num))}>{num}</SectionCard>)
+                <SectionCard key={i}
+                             onSelect={onSelectNumber && (() => onSelectNumber(num))}
+                             isMarked={markedNumbe===num}>
+                    {num}
+                </SectionCard>)
             )}
         </Section>
     );
--- src/components/SectionCard.jsx
+++ src/components/SectionCard.jsx
@@ -1,10 +1,12 @@
 import { Card, Col } from 'react-bootstrap';
 
 export function SectionCard(props) {
-    const { children, title,onSelect } = props;
+    const { children, title,onSelect,isMarked } = props;
+    const className = `m-2 p-2 shadow-sm text-center ${isMarked && "bg-warning" }`;
+
     return (
         <Col s={12} sm={6} md={4} lg={3} xl={2} xxl={2}>
-            <Card className="m-2 p-2 shadow-sm text-center" onClick={onSelect}>
+            <Card className={className} onClick={onSelect}>
                 <h4> {title}</h4>
                 {children}
             </Card>
