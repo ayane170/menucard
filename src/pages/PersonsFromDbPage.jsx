316fe8e0a3718742f0bd7e79b41ded0238e6cb94
Aya Nemri <r0987683@student.thomasmore.be>
2024-10-22 19:55:50 +0200

    search bar

--- src/pages/PersonsFromDbPage.jsx
+++ src/pages/PersonsFromDbPage.jsx
@@ -2,6 +2,8 @@ import {firestoreDB} from "../services/firebase.js";
 import {useCollectionData} from "react-firebase-hooks/firestore";
 import{collection} from 'firebase/firestore'
 import {Persons} from "../components/Persons.jsx";
+import {Form} from "react-bootstrap";
+import {useState} from "react";
 
 const personConverter = {
     toFirestore: undefined, //we will do this later
@@ -14,11 +16,18 @@ const personConverter = {
 export function PersonsFromDbPage(){
     const query = collection(firestoreDB,'persons').withConverter(personConverter);
     const [values,loading,error] = useCollectionData(query);
+    const [search, setSearch] = useState("");
     console.log({values,loading,error});
     return(
         <>
             <div className="mx-3">
-                <Persons title="Persons from database" persons={values} isInitiallyOpen={true}/>
+                <Form>
+                    <Form.Label>search</Form.Label>
+                    <Form.Control
+                        value={search}
+                        onChange={e => setSearch(e.target.value)}/>
+                </Form>
+                <Persons title="Personen uit database" persons={values?.filter(p => p.name.toLowerCase().includes(search.toLowerCase()))} isInitiallyOpen={true}/>
             </div>
         </>
     );
