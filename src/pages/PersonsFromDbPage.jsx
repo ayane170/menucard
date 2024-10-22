763782bcb31b2fc615a6767de92ca0f239bf087d
Aya Nemri <r0987683@student.thomasmore.be>
2024-10-22 19:45:28 +0200

    get unique id from database

--- src/pages/PersonsFromDbPage.jsx
+++ src/pages/PersonsFromDbPage.jsx
@@ -2,15 +2,21 @@ import {firestoreDB} from "../services/firebase.js";
 import {useCollectionData} from "react-firebase-hooks/firestore";
 import{collection} from 'firebase/firestore'
 
-
+const personConverter = {
+    toFirestore: undefined, //we will do this later
+    fromFirestore: function (snapshot, options) {
+        const data = snapshot.data(options);
+        return {...data, id: snapshot.id}
+    }
+};
 
 export function PersonsFromDbPage(){
-const query = collection(firestoreDB,'persons');
-const [values,loading,error] = useCollectionData(query);
-console.log({values,loading,error});
-return(
-    <>
-        <div>Persons from db</div>
-    </>
-);
+    const query = collection(firestoreDB,'persons').withConverter(personConverter);
+    const [values,loading,error] = useCollectionData(query);
+    console.log({values,loading,error});
+    return(
+        <>
+            <div>Persons from db</div>
+        </>
+    );
 }
